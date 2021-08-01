b1bc3fd34871c84945792f4f4ac3fd748ec9ca1a
bahizi123 <erickarinda5000@gmail.com>
2021-08-01 14:26:47 +0200

    updated on about page

---
+++ about.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Us Section Design</title>
+	<link rel="stylesheet" type="text/css" href="css/style2.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatics.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700$display=swap" rel="stylesheet">
+
+</head>
+<body>
+<section class="about">
+	<div class="main">
+		<img src="imageA.jpg">
+		<div class="about-text">
+			<h1>About Us</h1>
+			<h5>Cars Available <span>& Booking</span></h5>
+			<p>The average price of a Small rental in Kigali, Rwanda is $70. The cheapest time to rent a Small in Kigali, Rwanda is in February. The price is 26% lower than the rest of the year at just $53 per day. A Small rental is typically 27% less expensive than the average car rental in Kigali. The average price of a vehicle rental in Kigali can vary based on a number of factors including supply, popularity of the vehicle, and how far out you book your rental car.</p>
+			<button type="button">Let's Talk</button>
+		</div>
+	</div>
+</section> 
+</body>
+</html>
